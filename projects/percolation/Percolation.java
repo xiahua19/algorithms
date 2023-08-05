1ad741ba5b19bda01421883f71835533b92470df
xiahua19 <jx.zhang@pku.edu.cn>
2023-08-05 19:03:45 +0800

    finish project percolation

---
+++ projects/percolation/Percolation.java
@@ -0,0 +1,66 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+import java.lang.reflect.WildcardType;
+
+public class Percolation {
+    private boolean[][] sites;
+    private int openSitesNum;
+    private WeightedQuickUnionUF wqu;
+    private int sideLength;
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation (int n) {
+        sites = new boolean[n][n];
+        openSitesNum = 0;
+        wqu = new WeightedQuickUnionUF(n * n);
+        sideLength = n;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j){
+                sites[i][j] = false;
+            }
+        }
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open (int row, int col) {
+        sites[row][col] = true;
+        openSitesNum += 1;
+        if (row-1 >= 0 && isOpen(row-1, col)) {
+            wqu.union(getOrder(row, col), getOrder(row-1, col));
+        }
+        if (col-1 >= 0 && isOpen(row, col-1)){
+            wqu.union(getOrder(row, col), getOrder(row, col-1));
+        }
+        if (row+1 < sideLength && isOpen(row+1, col)){
+            wqu.union(getOrder(row, col), getOrder(row+1, col));
+        }
+        if (col+1 < sideLength && isOpen(row, col+1)){
+            wqu.union(getOrder(row, col), getOrder(row, col+1));
+        }
+    }
+
+    // get the order of site (row, col)
+    private int getOrder (int row, int col) {
+        return row*sideLength + col;
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen (int row, int col) {
+        return sites[row][col];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull (int row, int col) {
+        for (int i = 0; i < sideLength; ++i) {
+            if (wqu.find(getOrder(row, col)) == wqu.find(getOrder(0, i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites () {
+        return openSitesNum;
+    }
+}
---
+++ projects/percolation/PercolationStats.java
@@ -0,0 +1,91 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    private final int trials;
+    private final int n;
+    private double[] openSitesNum;
+    private Percolation perc;
+
+    // perform independent trials on an n-by-n grid
+    public  PercolationStats (int n, int trials){
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.trials = trials;
+        this.n = n;
+        this.openSitesNum = new double[trials];
+
+        simulation();
+    }
+
+    // reach percolation
+    private boolean reachPercolation() {
+        for (int i = 0; i < n; ++i) {
+            if (perc.isFull(n-1, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // get random (row, col) from unopened sites
+    private int[] getRandomRL() {
+        int row = StdRandom.uniformInt(0, n);
+        int col = StdRandom.uniformInt(0, n);
+        while (row >= n || col >= n || row < 0 || col < 0 || perc.isOpen(row, col)) {
+            row = StdRandom.uniformInt(0, n+1);
+            col = StdRandom.uniformInt(0, n+1);
+        }
+        return new int[] {row, col};
+    }
+
+    // start simulations
+    public void simulation () {
+        for (int t = 0; t < this.trials; ++t) {
+            perc = new Percolation(n);
+            while (! reachPercolation()) {
+                int[] rowCol = getRandomRL();
+                perc.open(rowCol[0], rowCol[1]);
+            }
+            StdOut.println("simulation " + t + ": " + perc.numberOfOpenSites());
+            openSitesNum[t] = (double) perc.numberOfOpenSites() / (this.n * this.n);
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean () {
+        return StdStats.mean(openSitesNum);
+    }
+
+    // sample stddev of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(openSitesNum);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo () {
+        return mean() - 1.96 * stddev() / Math.sqrt(trials);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi () {
+        return mean() + 1.96 * stddev() / Math.sqrt(trials);
+    }
+
+    // test client
+    public  static void main (String[] agrs) {
+        PercolationStats percStat = new PercolationStats(Integer.parseInt(agrs[0]), Integer.parseInt(agrs[1]));
+
+        double mean = percStat.mean();
+        double stddev = percStat.stddev();
+        double confidenceLo = percStat.confidenceLo();
+        double confidenceHi = percStat.confidenceHi();
+
+        StdOut.println("mean                    = " + mean);
+        StdOut.println("stddev                  = " + stddev);
+        StdOut.println("95% confidence interval = " + "[" + confidenceLo + ", " + confidenceHi + "]");
+    }
+}
