b6e7b1019db2a7ea5a50f6ca400474bdfe5ec0fb
xiahua19 <jx.zhang@pku.edu.cn>
2023-08-09 10:10:08 +0800

    init project collinear

---
+++ projects/collinear/FastCollinearPoints.java
@@ -0,0 +1,26 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class FastCollinearPoints {
+    // finds all line segments containing 4 or more points
+    public FastCollinearPoints(Point[] points) {
+
+    }
+
+    // the number of line sgements
+    public int numberOfSegments() {
+
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
