d547bccaaa44878a83daa55b8fbd0803d8b07843
xiahua19 <jx.zhang@pku.edu.cn>
2024-02-26 11:07:23 +0800

    finish PointSET.java

--- projects/proj06kdtree/PointSET.java
+++ projects/proj06kdtree/PointSET.java
@@ -9,6 +9,7 @@ import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.StdDraw;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class PointSET {
@@ -59,6 +60,14 @@ public class PointSET {
         if (rect == null) {
             throw new IllegalArgumentException("input Point2D is null");
         }
+        ArrayList<Point2D> range = new ArrayList<Point2D>();
+        for (Iterator<Point2D> it = set.iterator(); it.hasNext(); ) {
+            Point2D p = it.next();
+            if (rect.contains(p)) {
+                range.add(p);
+            }
+        }
+        return range;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
@@ -66,6 +75,20 @@ public class PointSET {
         if (p == null) {
             throw new IllegalArgumentException("input Point2D is null");
         }
+        if (set.isEmpty()) {
+            return null;
+        }
+        double minDist = Double.MAX_VALUE;
+        Point2D nearPoint = null;
+        for (Iterator<Point2D> it = set.iterator(); it.hasNext(); ) {
+            Point2D pp = it.next();
+            double dist = p.distanceTo(pp);
+            if (dist < minDist) {
+                minDist = dist;
+                nearPoint = pp;
+            }
+        }
+        return nearPoint;
     }
 
     // unit testing of the methods (optional)
